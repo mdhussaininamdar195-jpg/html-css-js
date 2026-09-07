47895bd6e56268ace2279a86ad7c7362b998b656
hussain-inamdar <mdhussaininamdar195@gmail.com>
2026-09-07 19:58:51 +0530

    add js ques  practice file

---
+++ js_prac/prac1.js
@@ -0,0 +1,30 @@
+//create an array of 10 numbers
+let arr=[1,2,3,4,5,6,7,8,9,10]
+console.log(arr)
+
+// filter even numbers
+console.log(arr.filter(e=>{
+    if(e%2===0){
+    return e;
+    }
+}))
+
+
+// // //double them using map
+console.log(arr.map(e=>{
+     return e*2;
+}))
+
+// // //calculate thier sum using reduce
+console.log(arr.reduce((a,b)=>{
+     return a+b;
+}))
+
+
+// //create async function
+async function hussain(){
+    console.log("hussain")
+}
+await hussain()
+
+
